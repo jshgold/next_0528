91c08833f27ea2d9e47de00592b06babf596a254
devho <jshgold93@gmail.com>
2026-05-28 15:01:31 +0900

    11강

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">
-        <header>
+    <html lang="ko" className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className="flex flex-col min-h-screen border-2 border-blue-500">
+        <header className="border-6 border-yellow-500">
           <nav className="flex">
             <Link href="/" className="p-2 rounded hover:bg-gray-100">
               메인
@@ -36,8 +36,8 @@ export default function RootLayout({
             </Link>
           </nav>
         </header>
-        <main>{children}</main>
-        <footer>푸터</footer>
+        <main className="flex-grow border-6 border-green-500 ">{children}</main>
+        <footer className="border-6 border-red-500">푸터</footer>
       </body>
     </html>
   );
