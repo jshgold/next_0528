9930231864f8d1d12aab507ab0e363a2a089d774
devho <jshgold93@gmail.com>
2026-05-28 14:23:04 +0900

    7강 8강 add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,12 +28,12 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <header>
           <nav className="flex">
-            <a href="/" className="p-2 rounded hover:bg-gray-100">
+            <Link href="/" className="p-2 rounded hover:bg-gray-100">
               메인
-            </a>
-            <a href="/posts" className="p-2 rounded hover:bg-gray-100">
+            </Link>
+            <Link href="/posts" className="p-2 rounded hover:bg-gray-100">
               글 목록
-            </a>
+            </Link>
           </nav>
         </header>
         <main>{children}</main>
